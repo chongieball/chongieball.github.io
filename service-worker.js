2992d7712ebb2d66454a322a6f7d4591affdb99b
Muhammad Iqbal <chongg1243@gmail.com>
2019-01-18 14:28:25 +0700

    remove unused code

--- service-worker.js
+++ service-worker.js
@@ -5,7 +5,6 @@ if (workbox)
 else
     console.log(`Workbox gagal dimuat`);
 
-const CACHE_NAME = "foo-match-v3";
 let urlsToCache = [
     { url: '/index.html', revision: '1' },
     { url: '/nav.html', revision: '1' },
@@ -22,24 +21,6 @@ let urlsToCache = [
     { url: '/js/layout.js', revision: '1' },
     { url: '/js/detail.js', revision: '1' },
     { url: '/js/moment.min.js', revision: '1' }
-    // { url: '/index.html', revision: '1' },
-    // { url: '/index.html', revision: '1' },
-    // { url: '/index.html', revision: '1' },
-    // "/pages/next.html",
-    // "/pages/past.html",
-    // "/pages/favorite.html",
-    // "/css/materialize.min.css",
-    // "/css/main.css",
-    // "/images/logo.png",
-    // "/js/materialize.min.js",
-    // "/js/nav.js",
-    // "/js/api.js",
-    // "/js/index.js",
-    // "/js/idb.js",
-    // "/js/db.js",
-    // "/js/layout.js",
-    // "/js/detail.js",
-    // "/js/moment.min.js"
 ];
 
 workbox.precaching.precacheAndRoute(urlsToCache)
@@ -55,50 +36,6 @@ workbox.routing.registerRoute(
         cacheName: 'pages'
     })
 );
-
-// self.addEventListener("install", function (event) {
-//     event.waitUntil(
-//         caches.open(CACHE_NAME).then(function (cache) {
-//             return cache.addAll(urlsToCache);
-//         })
-//     );
-// });
-
-self.addEventListener("fetch", function (event) {
-    let base_url = "https://api.football-data.org/v2/";
-
-    if (event.request.url.indexOf(base_url) > -1) {
-        event.respondWith(
-            caches.open(CACHE_NAME).then(function (cache) {
-                return fetch(event.request).then(function (response) {
-                    cache.put(event.request.url, response.clone());
-                    return response;
-                })
-            })
-        );
-    } else {
-        event.respondWith(
-            caches.match(event.request, { ignoreSearch: true }).then(function (response) {
-                return response || fetch(event.request);
-            })
-        )
-    }
-});
-
-
-self.addEventListener('activate', function (event) {
-    event.waitUntil(
-        caches.keys().then(function (cacheNames) {
-            return Promise.all(
-                cacheNames.map(function (cacheName) {
-                    if (cacheName != CACHE_NAME) {
-                        return caches.delete(cacheName)
-                    }
-                })
-            )
-        })
-    )
-})
 
 self.addEventListener('push', function(event) {
     let body;
